0d5a307671905d84f4af83a28aa6bc62491b778d
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-28 21:50:12 +0000

    Moved functions to input file. Imported setupInput function as well

--- play.js
+++ play.js
@@ -1,28 +1,14 @@
-const {connect} = require('./client');
-
 const net = require("net");
 const { stdin } = require('process');
 
-// setup interface to handle user input from stdin
-// \u0003 maps to ctrl+c input
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); 
-  return stdin;
-};
-
-const handleUserInput = function (input) {
-  if (input === '\u0003') {
-    process.exit();
-  }          
-};
+console.log("Connecting ...");
+connect();
 
 setupInput();
-handleUserInput();
+
 
 
 
