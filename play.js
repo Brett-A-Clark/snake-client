868a322ee9a56c3170c1c407e24567069ae1fd83
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-28 19:40:59 +0000

    Imported connect function

--- play.js
+++ play.js
@@ -1,21 +1,23 @@
+const {connect} = require('./client');
+
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: '50542', // PORT number here,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '135.23.223.133', // IP address here,
+//     port: '50542', // PORT number here,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
+//   conn.on('data', (data) => {
+//     console.log('Server says: ', data);
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
