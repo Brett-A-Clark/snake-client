1b7a1da337e46ca919e2bef2a35d836c2240fc5e
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-28 21:43:26 +0000

    Terminate input added to game

--- play.js
+++ play.js
@@ -1,23 +1,29 @@
 const {connect} = require('./client');
 
 const net = require("net");
+const { stdin } = require('process');
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '135.23.223.133', // IP address here,
-//     port: '50542', // PORT number here,
-//   });
+// setup interface to handle user input from stdin
+// \u0003 maps to ctrl+c input
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); 
+  return stdin;
+};
 
-//   conn.on('data', (data) => {
-//     console.log('Server says: ', data);
-//   });
+const handleUserInput = function (input) {
+  if (input === '\u0003') {
+    process.exit();
+  }          
+};
 
-//   return conn;
-// };
+setupInput();
+handleUserInput();
 
-// console.log("Connecting ...");
-// connect();
+
+
+
