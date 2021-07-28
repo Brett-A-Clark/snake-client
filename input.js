866922f5c90b20cd4449ede33233a9dcfddc2448
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-28 21:50:51 +0000

    Created file and moved functions from play file. exported setupInput function as well.

---
+++ input.js
@@ -0,0 +1,24 @@
+// setup interface to handle user input from stdin
+// \u0003 maps to ctrl+c input
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); 
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+  if (input === '\u0003') {
+    process.exit();
+  }          
+};
+
+setupInput();
+handleUserInput();
+
+module.exports = {
+  setupInput,
+};
