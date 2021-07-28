6ba61c0adec7d85a064df4d264756a735090399c
vagrant <b-a-clark@shaw.cagit config --global user.name Brett>
2021-07-28 20:49:11 +0000

    Move commands

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133', // IP address here,
     port: '50542', // PORT number here,
+    MoveUp: "Move: up"
   });
   conn.Name 
   
@@ -14,9 +15,12 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: BAC");
-  })
+  });
+
+  conn.write("Move: up");
   
   conn.on('data', (data) => {
+    "Mo"
     console.log('Server says: ', data);
   });
 
